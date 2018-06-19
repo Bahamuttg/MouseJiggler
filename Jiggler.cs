3eb16c13684c085c6bb2707bf679e80b2f2e5393
Tom <tom@tgcustompaint.com>
2018-06-19 14:12:18 -0500

    GPL Block

--- Jiggler.cs
+++ Jiggler.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/*
+    Mouse Jiggler
+    Copyright © 2018 Thomas George
+
+    This file is part of Mouse Jiggler.
+
+    Mouse Jiggler is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Mouse Jiggler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Mouse Jiggler.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 
